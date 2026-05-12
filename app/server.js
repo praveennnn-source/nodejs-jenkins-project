95cf715d590d4070191bbd80d215ea53e0ebd4e9
praveennnn-source <praveenprvn420@gmail.com>
2026-05-12 22:07:43 +0530

    Fix typo in response message

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOpsed Projectssssssssss 🚀");
+    res.send("Hello from Node.js DevOpsed Projectsssssssssss 🚀");
 });
 
 app.get('/health', (req, res) => {
