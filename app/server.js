50808321e1bad91b32330fdb6227999cd5eb6b4e
praveennnn-source <praveenprvn420@gmail.com>
2026-05-12 22:18:26 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOpsed Projectsssssssssgsss 🚀");
+    res.send("Hello from Node.js DevOpsed Projtsssssssssgsss 🚀");
 });
 
 app.get('/health', (req, res) => {
